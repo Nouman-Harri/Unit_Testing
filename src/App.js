47fdfbf10fa0a8c1705c6a9a434ee5882d77dda2
nouman <noumanharry7@gmail.com>
2023-03-09 01:54:31 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import SignupForm from './Components/App'
 
 export default function App() {
   return (
     <>
-      <SignupForm />
+      <SignupForm/>
     </>
   )
 }
