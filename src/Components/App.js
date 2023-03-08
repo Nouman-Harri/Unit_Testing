7fd83a7247378e0f1a0abbd7a1b4fea2a70bd84e
nouman <noumanharry7@gmail.com>
2023-03-09 01:44:53 +0500

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,127 @@
+import { useState } from "react";
+import "../index1.css"
+
+
+const SignupForm = () => {
+  const [formState, setFormState] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (formState.password !== formState.confirmPassword) {
+      alert("Passwords do not match!");
+      return;
+    }
+
+    console.log(formState);
+    setShowPopup(true);
+  };
+
+  const handleInputChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    setFormState({ ...formState, [name]: value });
+  };
+
+
+  return (
+    <div className="my-3">
+      <form onSubmit={handleSubmit} className="form_shape">
+        <div className="form-group">
+          <label htmlFor="email"><b>Email</b></label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            placeholder="Enter email"
+            value={formState.email}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="firstName"><b>First Name</b></label>
+          <input
+            type="text"
+            className="form-control"
+            id="firstName"
+            name="firstName"
+            placeholder="Enter first name"
+            value={formState.firstName}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="lastName"><b>Last Name</b></label>
+          <input
+            type="text"
+            className="form-control"
+            id="lastName"
+            name="lastName"
+            placeholder="Enter last name"
+            value={formState.lastName}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password"><b>Password</b></label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            placeholder="Password"
+            value={formState.password}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="confirmPassword"><b>Confirm Password</b></label>
+          <input
+            type="password"
+            className="form-control"
+            id="confirmPassword"
+            name="confirmPassword"
+            placeholder="Confirm password"
+            value={formState.confirmPassword}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary my-3 w-100">
+          Sign Up
+        </button>
+        {showPopup && (
+          <div className="popup">
+            <div className="popup-content">
+              <p className="text-danger"> <i><b>You have successfully created an account.!</b></i></p>
+              <button
+                className="close-button text-white bg-danger p-2 border rounded-top rounded-bottom"
+                onClick={() => setShowPopup(false)}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default SignupForm;
---
+++ src/SignupForm.test.js
@@ -0,0 +1,46 @@
+import { render, fireEvent } from "@testing-library/react";
+import SignupForm from "./SignupForm";
+
+describe("SignupForm", () => {
+  test("renders all input fields correctly", () => {
+    const { getByLabelText } = render(<SignupForm />);
+    expect(getByLabelText("Email")).toBeInTheDocument();
+    expect(getByLabelText("First Name")).toBeInTheDocument();
+    expect(getByLabelText("Last Name")).toBeInTheDocument();
+    expect(getByLabelText("Password")).toBeInTheDocument();
+    expect(getByLabelText("Confirm Password")).toBeInTheDocument();
+  });
+
+  test("submits the form successfully when all inputs are valid", () => {
+    const { getByLabelText, getByRole, getByText } = render(<SignupForm />);
+    const emailInput = getByLabelText("Email");
+    const firstNameInput = getByLabelText("First Name");
+    const lastNameInput = getByLabelText("Last Name");
+    const passwordInput = getByLabelText("Password");
+    const confirmPasswordInput = getByLabelText("Confirm Password");
+
+    fireEvent.change(emailInput, { target: { value: "test@test.com" } });
+    fireEvent.change(firstNameInput, { target: { value: "John" } });
+    fireEvent.change(lastNameInput, { target: { value: "Doe" } });
+    fireEvent.change(passwordInput, { target: { value: "password" } });
+    fireEvent.change(confirmPasswordInput, { target: { value: "password" } });
+
+
+    fireEvent.click(getByRole("button"));
+
+    expect(getByText("You have successfully created an account.")).toBeInTheDocument();
+  });
+
+  test("displays an error message when the passwords do not match", () => {
+    const { getByLabelText, getByRole, getByText } = render(<SignupForm />);
+    const passwordInput = getByLabelText("Password");
+    const confirmPasswordInput = getByLabelText("Confirm Password");
+
+    fireEvent.change(passwordInput, { target: { value: "password" } });
+    fireEvent.change(confirmPasswordInput, { target: { value: "wrongpassword" } });
+
+    fireEvent.click(getByRole("button"));
+
+    expect(getByText("Passwords do not match!")).toBeInTheDocument();
+  });
+});
